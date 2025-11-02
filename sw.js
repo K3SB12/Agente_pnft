477c391ed98cd513e0b2526afd44d75962dc3a75
K3SB12 <143125631+K3SB12@users.noreply.github.com>
2025-11-02 09:19:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,3 @@
-const CACHE_NAME = 'mi-al-ia-da-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
-];
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(()=>self.skipWaiting())
-  );
-});
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-});
+self.addEventListener("install", event => { self.skipWaiting(); });
+self.addEventListener("activate", event => { self.clients.claim(); });
+self.addEventListener("fetch", event => { /* dejamos pasar todo por defecto */ });
